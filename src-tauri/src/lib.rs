a146c0eefab4c8d5523b4cb1fbb6dbdb832c4bd5
ppp3ppj <ppp.3ppj@gmail.com>
2025-12-19 12:27:53 +0700

    Fix white screen issue on app launch by clearing cache on Linux

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 use tauri_plugin_sql::{Migration, MigrationKind};
+use std::fs;
+use tauri::Manager;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -28,6 +30,20 @@ pub fn run() {
         )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(target_os = "linux")]
+            {
+                let cache_dir = app.path().cache_dir()?;
+                let package_info = app.package_info();
+                let app_name = package_info.name.as_str();
+                let app_cache = cache_dir.join(app_name);
+                if app_cache.exists() {
+                    let _ = fs::remove_dir_all(&app_cache);
+                }
+                
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
